e65728800e7af76a134235043754b557dbdf3f97
KO <kkw9075@naver.com>
2025-12-02 17:41:12 +0900

    검색바 작업

---
+++ front/src/components/common/SearchBar.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { SearchOutlined } from "@ant-design/icons";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.5vw;
+  padding: 1.5vh 3vw;
+  border-radius: 50vh;
+  background: rgba(0, 0, 0, 0.35);
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  overflow: hidden;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  border: none;
+  background: transparent;
+  color: #f3c7d1;
+  outline: none;
+  font-size: 2vh;
+
+  &::placeholder {
+    color: #a67aa0;
+  }
+`;
+
+const StyledIcon = styled(SearchOutlined)`
+  font-size: 2.5vh;
+  color: #f3c7d1;
+`;
+
+const SearchBar = () => {
+  return (
+    <Wrapper>
+      <StyledIcon />
+      <Input placeholder="검색" />
+    </Wrapper>
+  );
+};
+
+export default SearchBar;
