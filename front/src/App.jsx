e9d3ba72d1101702b79337204d46779cf2a3d9bf
KO <kkw9075@naver.com>
2025-12-05 11:24:57 +0900

    App.jsx 수정

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainLayout from "./components/common/MainLayout";
 import BottomNav from "./components/common/BottomNav";
+import TestPage from "./pages/TestPage";
+import SearchPage from "./pages/common/SearchPage";
+
+
 function App() {
   return (
     <BrowserRouter>
@@ -23,61 +27,7 @@ function App() {
           }
         />
 
-        {/* ProfilePage - 커스텀 헤더 + BottomNav */}
-        <Route
-          path="/profile"
-          element={
-            <>
-              <ProfilePage 
-                username="EAST__SEA"
-                displayName="최동해"
-                profileImage="https://via.placeholder.com/150"
-                bio="동해조아"
-                posts={15}
-                followers="7k"
-                following={3}
-              />
-              <BottomNav 
-                onHome={() => console.log('홈')}
-                onSearch={() => console.log('검색')}
-                onAdd={() => console.log('추가')}
-                onProfile={() => console.log('프로필')}
-                onSetting={() => console.log('설정')}
-              />
-            </>
-          }
-        />
-        
-        {/* BadgeTestPage - 뱃지 테스트 */}
-        <Route
-          path="/badge-test"
-          element={
-            <MainLayout current="home">
-              <BadgeTestPage />
-            </MainLayout>
-          }
-        />
-
-        {/* LoadingTestPage - 로딩 테스트 */}
-        <Route
-          path="/loading-test"
-          element={
-            <MainLayout current="home">
-              <LoadingTestPage />
-            </MainLayout>
-          }
-        />
-
-        {/* TestPage2 - 버튼 테스트 */}
-        <Route
-          path="/test2"
-          element={
-            <MainLayout current="home">
-              <TestPage2 />
-            </MainLayout>
-          }
-        />
-        
+ 
         {/* 다른 페이지들 MainLayout 사용 */}
         <Route
           path="/"
