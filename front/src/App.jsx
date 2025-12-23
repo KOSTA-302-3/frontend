291282763978b8a5fdd707f8d577c722e5cdd497
donghae <choi7gold@gmail.com>
2025-12-23 17:23:35 +0900

    하드코딩 주소들 env로 변경

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,11 +2,15 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { increaseUnreadCount } from "./store/slices/notificationSlice";
+const wsNotificationIp = import.meta.env.VITE_WS_NOTIFICATION_IP || "";
 
 function App() {
   const dispatch = useDispatch();
+
+  const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+
   useEffect(() => {
-    const socket = new WebSocket("ws://192.168.0.19:8080/ws/notification");
+    const socket = new WebSocket(`${protocol}${wsNotificationIp}`);
 
     socket.onmessage = () => {
       console.log("Notification WS message received");
